4c8fadde95b16385aec26ca0eb29beefd65ada4f
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2025-10-08 19:24:06 +0200

    indent-blankline: Oppdater config for versjon 2->3.

--- nvim/init.lua
+++ nvim/init.lua
@@ -127,13 +127,13 @@ require('lazy').setup({
   },
   {
     -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
+    "lukas-reineke/indent-blankline.nvim",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
   },
 
   -- "gc" to comment visual regions/lines
